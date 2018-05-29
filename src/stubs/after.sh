008bb06a6243ec6ea18a325b5f9b92a2e1d47f98
shuangxijiang <jiangsx@kingnet.com>
2018-05-29 09:45:19 +0800

    解决内存问题

--- src/stubs/after.sh
+++ src/stubs/after.sh
@@ -3,3 +3,6 @@
 # If you would like to do some extra provisioning you may
 # add any commands you wish to this file and they will
 # be run after the Homestead machine is provisioned.
+
+
+sudo sh /home/vagrant/stop_free_jb.sh
